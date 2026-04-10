9cd24a7bf5d7ac34f9b7ed887edbd4ecd52c8557
k1000dai <chiyodakku1000@gmail.com>
2026-04-10 12:09:00 +0900

    add skhd yabai

--- home.nix
+++ home.nix
@@ -40,16 +40,11 @@
   # plain files is through 'home.file'.
   home.file = {
       ".tmux.conf".source = ./.tmux.conf;
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
+  };
 
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+  xdg.configFile = {
+      "yabai".source = ./config/yabai;
+      "skhd".source = ./config/skhd;
   };
 
   # Home Manager can also manage your environment variables through
