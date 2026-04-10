49e7f4fff2bf171033f3e6a99bb3f34e32d4c9cb
k1000dai <chiyodakku1000@gmail.com>
2026-04-10 13:11:28 +0900

    lazygit

--- home.nix
+++ home.nix
@@ -6,42 +6,25 @@
   home.username = "kohei";
   home.homeDirectory = "/Users/kohei";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
   # release notes.
   home.stateVersion = "25.11"; # Please read the comment before changing.
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
+    #shell
+    pkgs.zsh
+
     pkgs.fzf
     pkgs.neovim
     pkgs.bat
     pkgs.tmux
     #git
     pkgs.git
+    pkgs.git-lfs
     pkgs.ghq
     pkgs.gh
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
+    pkgs.lazygit
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
@@ -53,6 +36,7 @@
   xdg.configFile = {
       "yabai".source = ./config/yabai;
       "skhd".source = ./config/skhd;
+      "lazygit".source = ./config/lazygit;
   };
 
   # Home Manager can also manage your environment variables through
