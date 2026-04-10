66908505085a9dc8db510ba86476d07de1e4b0ea
k1000dai <chiyodakku1000@gmail.com>
2026-04-10 12:23:20 +0900

    fzf,gh,ghq

--- home.nix
+++ home.nix
@@ -18,8 +18,13 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
+    pkgs.fzf
     pkgs.bat
+    pkgs.tmux
+    #git
     pkgs.git
+    pkgs.ghq
+    pkgs.gh
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
